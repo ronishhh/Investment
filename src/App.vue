34da18fccc232209e66e65bf3594cbef79a1e52f
Ronish <stharonish32@gmail.com>
2024-08-02 16:33:31 +0545

    add: our team aboutUs complete

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,9 @@
   <Main />
 </template>
 <script setup>
-import Main from "./layout/Main.vue"
+import Main from "./layout/Main.vue";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 </script>
--- src/components/About/Team.vue
+++ src/components/About/Team.vue
@@ -19,20 +19,38 @@
         </p>
       </div>
       <div class="lower border-t-2 w-full h-1/2 relative">
-        <div class="h-full absolute top-[-150px] left-20">
-          <div class="h-full w-full bg-green-300">
-            <div class="h-full w-56 bg-gray-200">
-              <img
-                src="https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg"
-                alt="team"
-                class="w-full h-full object-cover"
-              />
-            </div>
-            <span class="text-lg font-medium">John Doe</span><br />
-            <span class="text-md font-medium">CEO & Founder</span>
+        <div class="absolute top-[-150px] px-20 h-[290px] w-full">
+          <div class="w-full h-full flex justify-between">
+            <swiper
+              class="w-full h-full"
+              :slides-per-view="4"
+              :space-between="110"
+              :pagination="{ clickable: true }"
+            >
+              <swiper-slide v-for="i in 8" :key="i">
+                <div class="h-56 w-56 bg-gray-200 team">
+                  <img
+                    src="https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg"
+                    alt="team"
+                    class="w-full h-full object-cover"
+                  />
+                </div>
+                <span class="text-lg font-medium">John Doe</span><br />
+                <span class="text-md font-medium">CEO & Founder</span>
+              </swiper-slide>
+            </swiper>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script setup>
+import { Swiper, SwiperSlide } from "swiper/vue";
+</script>
+<style>
+.team {
+  /* box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; */
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+}
+</style>
