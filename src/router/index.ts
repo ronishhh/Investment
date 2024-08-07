8f486d899b754f3248f95788a4c025cc3fa5e93a
Ronish <stharonish32@gmail.com>
2024-08-07 17:58:26 +0545

    update: routes and scrollbehaviour on unsavedPosition

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,41 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
+  },
+  {
+    path: "/blogs",
+    name: "blogs",
+    component: () => import("../views/BlogView.vue"),
+  },
+  {
+    path: "/blogs/:id",
+    name: "blog-detail",
+    component: () => import("../views/BlogDetailView.vue"),
+    props: true,
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/blogs",
-      name: "blogs",
-      component: () => import("../views/BlogView.vue"),
-    },
-    {
-      path: "/blogs/:id",
-      name: "blog-detail",
-      component: () => import("../views/BlogDetailView.vue"),
-      props: true,
-    },
-  ],
+  routes: routes,
+  scrollBehavior(from, to, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+    
+  }
 });
 
 export default router;
