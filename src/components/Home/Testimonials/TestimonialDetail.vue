bd734f16f6c7f732a658539216f9a49e522abcad
Ronish <stharonish32@gmail.com>
2024-08-08 17:35:36 +0545

    fix: testimonials responsiveness

--- src/components/Home/Testimonials/TestimonialDetail.vue
+++ src/components/Home/Testimonials/TestimonialDetail.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="py-12">
-    <div class="card lg:card-side shadow-xl">
-      <figure class="w-[200px] h-auto">
+    <div class="card lg:card-side shadow-xl m-0">
+      <figure class="lg:w-[200px] w-[300px] lg:h-auto h-[250px]">
         <img
           src="https://i.pinimg.com/564x/08/31/2e/08312e96e86c9bfc560a82359b5d1412.jpg"
           alt="Album"
           class="w-full h-full object-cover"
         />
       </figure>
-      <div class="card-body w-[500px] text-left">
+      <div class="card-body lg:w-[500px] w-[300px] text-left">
         <div class="flex justify-between items-center rating-xs">
-          <span class="text-sm text-gray-500">Jan 8, 2023</span>
+          <span class="lg:text-sm text-xs text-gray-500">Jan 8, 2023</span>
           <div class="rating">
             <input
               type="radio"
@@ -39,8 +39,8 @@
             />
           </div>
         </div>
-        <h5 class="card-title h5">It was a very good experience</h5>
-        <p class="text-sm text-justify">
+        <h5 class="card-title lg:h5 h4">It was a very good experience</h5>
+        <p class="lg:text-sm text-xs text-justify m-0">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eos
           ratione adipisci, aliquam commodi possimus impedit maxime soluta magni
           eius repellat error eum sunt rerum similique nesciunt facilis, ipsum
